fdac6473853750e6eacb125e2d8b6f303a68efa5
Rolena Williams <rolenawilliams@Rolenas-MacBook-Air.local>
2026-03-03 23:09:40 -0500

    Details Screen updated to include name ingredients and instructions

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -16,6 +16,41 @@ class DetailsScreen extends StatelessWidget {
             // Hero image
             Image.asset(recipe.imagePath, height: 220, width: double.infinity, fit: BoxFit.cover),
             // Name, Ingredients, Instructions…
+            const SizedBox(height: 16),
+            
+            Padding( //Name
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(recipe.name, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold,))
+            ),
+            const SizedBox(height: 16),
+
+            const Padding( //Ingredients
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Text('Ingredients', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,))
+            ),
+            const SizedBox(height: 8),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: recipe.ingredients.map((ingredient) => Text("• $ingredient"),).toList(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            
+            const Padding( //Instructions
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Text('Instructions', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),
+              ),
+            ),
+            const SizedBox(height: 8),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(recipe.instructions, style: const TextStyle(fontSize: 16),),
+            ),
+            const SizedBox(height: 24),
           ],
         ),
       ),
